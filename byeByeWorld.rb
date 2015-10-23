08f08953954443d0731bb27ff9e51276740e0415
alu0100502107 <alu0100502107@ull.edu.es>
2015-10-23 13:55:01 +0000

    Creado el programa byeByeWorld.rb en Ruby para usar con PRY

---
+++ byeByeWorld.rb
@@ -0,0 +1,17 @@
+#byeByeWorld.rb
+require 'pry'
+
+#define a method
+def byeBye()
+    puts "Bye bye world!!!"
+end
+
+#set x to 10
+x = 10
+
+#start a REPL session
+binding.pry
+
+#program resumes here (after pry session)
+puts "program resumes here. Value of is: #{x}."
+
